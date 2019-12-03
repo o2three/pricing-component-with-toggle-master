012c14e95f5b2cf6186db1d5502f3398bd9aacef
o2three <ayshui@hotmail.co.uk>
2019-12-03 20:25:35 +0000

    Initial

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const checkBox = document.querySelector(".checkbox");
+const toggleSwitch = document.querySelector(".switch");
+const basic = document.querySelector(".basic");
+const professional = document.querySelector(".professional");
+const master = document.querySelector(".master");
+
+loadEventListeners();
+
+function loadEventListeners() {
+  toggleSwitch.addEventListener("click", addText);
+}
+
+function addText() {
+  if (checkBox.checked === true) {
+    basic.textContent = "199.99";
+    professional.textContent = "249.99";
+    master.textContent = "399.99";
+  } else {
+    basic.textContent = "19.99";
+    professional.textContent = "24.99";
+    master.textContent = "39.99";
+  }
+}
