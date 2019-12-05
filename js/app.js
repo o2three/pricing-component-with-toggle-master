f981be14a2ae56ea176a84baf8c484b4d4c96acc
o2three <ayshui@hotmail.co.uk>
2019-12-05 12:44:23 +0000

    Last updated on 05-12-2019

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ loadEventListeners();
 
 function loadEventListeners() {
   toggleSwitch.addEventListener("click", addText);
+  checkBox.addEventListener("keyup", addText2);
 }
 
 function addText() {
@@ -19,5 +20,19 @@ function addText() {
     basic.textContent = "19.99";
     professional.textContent = "24.99";
     master.textContent = "39.99";
+  }
+}
+
+function addText2(e) {
+  if (e.code === "Space") {
+    if (checkBox.checked === true) {
+      basic.textContent = "199.99";
+      professional.textContent = "249.99";
+      master.textContent = "399.99";
+    } else {
+      basic.textContent = "19.99";
+      professional.textContent = "24.99";
+      master.textContent = "39.99";
+    }
   }
 }
